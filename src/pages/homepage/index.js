8b13694ac7333aae626932757f58b1ec67b818da
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2023-10-21 10:49:41 +0500

    songs fetched from spotify using api

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,4 +1,5 @@
 // ** Images imports
+import { useState } from "react";
 import bg from "../../assets/images/background_photo_desktop.jpg";
 import Playlist from "../../components/playlist";
 
@@ -6,14 +7,24 @@ import Playlist from "../../components/playlist";
 import SearchBox from "../../components/searchbox";
 import TracksCard from "../../components/trackListCard";
 
+// ** API imports
+import Spotify from "../../features/spotifyApi";
+
 const HomePage = () => {
+  // ** States
+  const [searchResults, setSearchResults] = useState([]);
+  searchResults && console.log(searchResults, "songs");
+
+  const search = (text) => {
+    Spotify.search(text).then(setSearchResults);
+  };
   return (
     <>
       <div
         className="bg-cover bg-no-repeat bg-center"
         style={{ backgroundImage: `url(${bg})` }}
       >
-        <SearchBox />
+        <SearchBox search={search} />
         <div className="flex flex-col justify-around md:flex-row md:justify-center items-center  ">
           <div className="m-[12px]">
             <TracksCard />
