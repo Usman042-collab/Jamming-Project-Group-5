ff15457b22251dc79f4e43b61262762b427da855
warda rasheed <97965868+wardaz@users.noreply.github.com>
2023-10-24 20:23:04 +0500

    Changes in some line

--- src/components/trackListCard/Track.js
+++ src/components/trackListCard/Track.js
@@ -1,12 +1,22 @@
+const Track = (props) => {
+  const { name, artist, album, addButton } = props;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="Track">
+        <h3>{name}</h3>
+        <p>
+          {artist} | <span>{album}</span>
+        </p>
+      </div>
 
-
-const Track=({name, artist, album})=>
-{
-   return(
-    <div className="Track" >
-    <h3>{name}</h3>
-    <p>{artist} | <span>{album}</span></p>
+      <div>
+        <button
+          className="bg-transparent-500 px-3 ms-2 py-1 rounded-md"
+          onClick={() => addButton(name)}
+        >
+          +
+        </button>
+      </div>
     </div>
-   )
-}
-export default Track;
+  );
+};
